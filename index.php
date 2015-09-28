73cae61f6e28376eed9947dd9c67cff87e4e8f6f
MatasT <turskismatas@gmail.com>
2015-09-28 18:04:56 +0300

    create

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/fontawesome/4.3.0/css/font-awesome.min.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body ng-app>
+
+    <div class="container">
+
+        <div ng-include="'header.html'"></div>
+
+    </div>
+    <div class="container downhead">
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="strings.php" class="thumbnail">
+                    <img src="photo/strings.jpg" alt="...">
+                    <div class="typename">Strings</div>
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="#" class="thumbnail">
+                    <img src="photo/keyboards.jpg" alt="...">
+                    <div class="typename">Keyboards</div>
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="#" class="thumbnail">
+                    <img src="photo/percussion.jpg" alt="...">
+                    <div class="typename">Percussion</div>
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="#" class="thumbnail">
+                    <img src="photo/brass.jpg" alt="...">
+                    <div class="typename">Brass</div>
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="#" class="thumbnail">
+                    <img src="photo/recording.jpg" alt="...">
+                    <div class="typename">Recording</div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ strings.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/fontawesome/4.3.0/css/font-awesome.min.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body ng-app>
+
+    <div class="container">
+
+        <div ng-include="'header.html'"></div>
+
+    </div>
+    <div class="container downhead">
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+            
+        </div>
+    </div>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+
+</html>
